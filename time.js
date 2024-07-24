f57ac2594b7f8dd0f0afbf5aaf19562194ad1298
bonn9494 <drisskais98@gmail.com>
2024-07-24 17:17:53 +0100

    Update time.js

--- time.js
+++ time.js
@@ -3,53 +3,6 @@ let button = document.getElementById("button")
 button.addEventListener("click",getAge)
 let text = document.getElementById("result")
 
-/*function getAge(){
-    let date = new Date(input.value)
-
-    let birthdayDay = date.getDate()
-    let birthdayMonth = date.getMonth()+1
-    let birthdayYear = date.getFullYear()
-    
-    let todayDate = new Date()
-
-    let todayDay = todayDate.getDate()
-    let todayMonth = todayDate.getMonth()+1
-    let todayYear = todayDate.getFullYear()
-    
-    let ageDay = todayDay - birthdayDay
-    let ageMonth = todayMonth - birthdayMonth
-    let ageYear = todayYear - birthdayYear
-
-    if(birthdayDay>todayDay){
-        ageMonth = ageMonth - 1
-        if(ageMonth%2==0){
-            ageDay = ageDay + 30
-            if(ageDay>31){
-                ageDay = ageDay - 31
-                ageMonth = ageMonth + 1
-            }
-        }
-    }
-        else{
-            ageDay = ageDay + 31
-            if(ageDay>31){
-                ageDay = ageDay - 31
-                ageMonth = ageMonth + 1
-        }
-    if(birthdayMonth>todayMonth){     
-        ageMonth = ageMonth + 11  
-        ageYear = ageYear - 1
-        if(ageMonth>12){
-            ageMonth = ageMonth - 12
-            ageYear = ageYear + 1
-        }
-
-    }
-    
-}   
-
-    text.innerHTML = `You are ${ageYear} years old, ${ageMonth} months and ${ageDay} days`
-}*/
 function getAge() {
     let date = new Date(input.value);
 
